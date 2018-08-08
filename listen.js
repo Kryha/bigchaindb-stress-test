76e24c3e9b894ec7e9d4c3f53b9d6a681be104e1
Dan Acristinii <dan@acristinii.com>
2018-08-08 12:46:57 +0200

    gets spawned by test.py

--- listen.js
+++ listen.js
@@ -9,20 +9,20 @@ var i = 0
 var j = 0
 var start
 var end
-console.log("Test ready")
+// console.log("Test ready")
 ws.on('message', (data) => {
-    j++ 
+    j++
     if (j==1) {
         start = new Date(); console.log("Start time: ",start)
-    }   
+    }
     else if (j==param) {
         end = new Date()
-        console.log("End time: ",end)
-        console.log("Received all transactions in %ds seconds",(end - start)/1000)
-        console.log("Total number of transactions: ",j)
-        console.log("Transactions per second throughput is %dtxs",j/((end - start)/1000))
+        console.log("End time: d.",end)
+        console.log("Received all transactions in: e. %ds seconds",(end - start)/1000)
+        console.log("Total number of transactions: f. ",j)
+        console.log("Transactions per second throughput is: g. %dtxs",j/((end - start)/1000))
         ws.terminate()
         process.exit();
     };
-     
+
 });
